0bb3b565cca508328a43660ce77a973f18aa99b7
Dikshuy <dikshant1212hsr@gmail.com>
2022-12-08 11:59:03 +0900

    added acceleration dynamics

--- predictor.py
+++ predictor.py
@@ -7,7 +7,7 @@ TO DO:
 1. add the stopping condition of the collision ball instead of only one direction   # DONE
 2. what if the vehicle have actually passed the collision scenario                  # DONE
 -> dynamics update
-1. include acceleration in the dynamics
+1. include acceleration in the dynamics                                             # DONE
 2. check the velocity profile update system
 3. angular velocity and steering angle dynamics
 4. inclusion of curvy roads and intersection scenario
@@ -36,14 +36,14 @@ tol = 0.3                                       # tolerance value for proximity 
 # for car
 start_pos_car = 0
 x_car = [start_pos_car]*future_horizon
-u_car = 3                                       # initial velocity of the car    
-a_car = 0                                       # acceleration of the car
+u_car = 2.5                                     # initial velocity of the car    
+a_car = 0.5                                     # acceleration of the car
 
 # for pedestrian
 start_pos_ped = -5
 y_ped = [start_pos_ped]*future_horizon
-u_ped = 0.5                                     # initial velocity of the pedestrian
-a_ped = 0                                       # acceleration of the pedestrian
+u_ped = 0.4                                     # initial velocity of the pedestrian
+a_ped = 0.1                                     # acceleration of the pedestrian
 
 # plotting 
 y_car = [0]*future_horizon 
@@ -70,25 +70,31 @@ def stop(x_car, y_car, x_ped, y_ped):
 ### MERGE
 while stop(x_car,y_car,x_ped,y_ped):
     vel_profile_car = []
+    acc_profile_car = []
     vel_profile_ped = []
+    acc_profile_ped = []
     for _ in range(horizon):
         noise_car = np.random.random()          # noise in the velocity profile of car
         vel_profile_car.append(u_car+noise_car)
+        acc_profile_car.append(a_car+np.random.choice([-1,1])*np.random.random()/2)
         noise_ped = np.random.random()          # noise in the velocity profile of pedestrian
         vel_profile_ped.append(u_ped+noise_ped/10)
+        acc_profile_ped.append(a_ped+np.random.choice([-1,1])*np.random.random()/10)
 
     # car dynamics
     avg_vel_car = np.mean(vel_profile_car)
+    avg_acc_car = np.mean(acc_profile_car)
     # update the x_car[1] by the actual position of the car
-    x_new = x_car[1] + np.multiply(avg_vel_car, t) + 0.5*a_car*np.square(t)
+    x_new = x_car[1] + np.multiply(avg_vel_car, t) + np.multiply(0.5*avg_acc_car,np.square(t))
     x_car = x_new
 
     # pedestrian dynamics
     avg_vel_ped = np.mean(vel_profile_ped)
+    avg_acc_ped = np.mean(acc_profile_ped)
     # update the y_ped[1] by the actual position of the pedestrian
-    y_new = y_ped[1] + np.multiply(avg_vel_ped, t) + 0.5*a_ped*np.square(t)
+    y_new = y_ped[1] + np.multiply(avg_vel_ped, t) + np.multiply(0.5*avg_acc_ped, np.square(t))
     y_ped = y_new
-
+    
     # plotting
     plt.plot(x_car, y_car)
     plt.plot(x_car[1], y_car[1], marker='>', markersize=5)
