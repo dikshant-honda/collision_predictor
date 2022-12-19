61fc2f1f32afd8f37a732e7d6fa0f76bd9fc3913
Dikshuy <dikshant1212hsr@gmail.com>
2022-12-19 16:08:15 +0900

    msg folder for variables usage

---
+++ msg.py
@@ -0,0 +1,33 @@
+class traffic_msgs:
+    def CenterLanes(self):
+        pass
+
+    def PathWithSpeed(self):
+        pass
+    
+    def Prediction(self):
+        pass
+
+    def PredictionArray(self):
+        pass
+
+    def PredictionLanes(self):
+        pass
+
+    def Waypoint(self):
+        pass
+
+    def WaypointArray(self):
+        pass
+
+    def IntentionTrajectory(self):
+        pass
+
+    def PerceptionLanes(self):
+        pass
+
+    def VehicleStateArray(self):
+        pass
+
+    def VehicleState(self):
+        pass
