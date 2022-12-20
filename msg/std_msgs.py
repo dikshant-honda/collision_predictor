e840b3b2a5236c6ce0c8c0cf65c238c51e92be50
Dikshuy <dikshant1212hsr@gmail.com>
2022-12-20 18:39:53 +0900

    added frame id

--- msg/std_msgs.py
+++ msg/std_msgs.py
@@ -3,5 +3,6 @@ import time
 class Header:
     def __init__(self, seq=None, stamp=None, frame_id=None):
         self.seq = seq                                           # uint32
-        self.stamp = time.time()                                       # time stamp
-        self.frame_id = frame_id 
+        stamp = time.time()
+        self.stamp = stamp                                       # time stamp
+        self.frame_id = "/base_link" 
