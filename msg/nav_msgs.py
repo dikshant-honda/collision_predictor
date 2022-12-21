7302c4653db4f72f7df6035cb4ec4214978b18ba
Dikshuy <dikshant1212hsr@gmail.com>
2022-12-21 09:52:02 +0900

    posestamped list added

--- msg/nav_msgs.py
+++ msg/nav_msgs.py
@@ -4,7 +4,10 @@ import geometry_msgs
 class Path:
     def __init__(self, header, posestamped):
         self.header = std_msgs.Header(header.seq, header.stamp, header.frame_id)
-        self.poses = geometry_msgs.PoseStamped(posestamped.header, posestamped.pose)
+        pose_arr = []
+        for i in range(len(posestamped)):
+            pose_arr.append(geometry_msgs.PoseStamped(posestamped.header, posestamped.pose))
+        self.poses = pose_arr
 
 # TEST
 # pt = geometry_msgs.Point()
