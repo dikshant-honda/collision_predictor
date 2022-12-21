6b3fef8f52002763449244380cc40ff8498382b5
Dikshuy <dikshant1212hsr@gmail.com>
2022-12-21 17:56:35 +0900

    redundant files removed

--- msg/std_msgs.py
+++
@@ -1,8 +0,0 @@
-import time
-
-class Header:
-    def __init__(self, seq=None, stamp=None, frame_id=None):
-        self.seq = seq                                           # uint32
-        stamp = time.time()
-        self.stamp = stamp                                       # time stamp
-        self.frame_id = "/base_link" 
