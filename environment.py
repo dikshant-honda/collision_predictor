dc71e14650107d61507747d3196cc4824048d41d
Dikshuy <dikshant1212hsr@gmail.com>
2022-12-21 17:55:39 +0900

    modified environment

--- environment.py
+++ environment.py
@@ -49,24 +49,39 @@ def get_spline(x0, x1, y0, y1, theta0, theta1, steps=100):
 
 	return x, y
 
-x1, y1, bound1_1, bound1_2 = get_straight(0,0,5,0,0)            # horizontal road 
-x2, y2, bound2_1, bound2_2 = get_straight(6,1,6,6,np.pi/2)      # vertical road
-x3, y3, bound3_1, bound3_2 = get_straight(7,0,12,0,0)           # horizontal road
-x4, y4, bound4_1, bound4_2 = get_straight(6,-1,6,-6,-np.pi/2)    # vertical road
-plt.plot(x1, y1, 'b--', markersize=1)
-plt.plot(x1, bound1_1, 'k')
-plt.plot(x1, bound1_2, 'k')
-plt.plot(x2, y2, 'b--', markersize=1)
-plt.plot(bound2_1, y2, 'k')
-plt.plot(bound2_2, y2, 'k')
-plt.plot(x3, y3, 'b--', markersize=1)
-plt.plot(x3, bound3_1, 'k')
-plt.plot(x3, bound3_2, 'k')
-plt.plot(x4, y4, 'b--', markersize=1)
-plt.plot(bound4_1, y4, 'k')
-plt.plot(bound4_2, y4, 'k')
-# x, y = get_spline(0,3,0,3,np.pi/2,np.pi/9)
-# x1,y1 = get_spline(3,6,3,0,np.pi/9,np.pi/5)
-# plt.plot(x,y)
-# plt.plot(x1,y1)
-plt.show()
+def register_lane(x, y,count):
+    lane_center = [x, y]
+    id = count
+    return lane_center, id
+
+if __name__ == '__main__':
+    x1, y1, bound1_1, bound1_2 = get_straight(-5,0,5,0,0)            # horizontal road 
+    x2, y2, bound2_1, bound2_2 = get_straight(6,1,6,11,np.pi/2)      # vertical road
+    x3, y3, bound3_1, bound3_2 = get_straight(7,0,17,0,0)           # horizontal road
+    x4, y4, bound4_1, bound4_2 = get_straight(6,-1,6,-11,-np.pi/2)    # vertical road
+
+    # lanes
+    count = 0
+    lane_center1, id1 = register_lane(x1, y1, count)
+    lane_center2, id2 = register_lane(x2, y2, count+1)
+    lane_center3, id3 = register_lane(x3, y3, count+2)
+    lane_center4, id4 = register_lane(x4, y4, count+4)
+
+    # plotting
+    plt.plot(x1, y1, 'b--', markersize=1)
+    plt.plot(x1, bound1_1, 'k')
+    plt.plot(x1, bound1_2, 'k')
+    plt.plot(x2, y2, 'b--', markersize=1)
+    plt.plot(bound2_1, y2, 'k')
+    plt.plot(bound2_2, y2, 'k')
+    plt.plot(x3, y3, 'b--', markersize=1)
+    plt.plot(x3, bound3_1, 'k')
+    plt.plot(x3, bound3_2, 'k')
+    plt.plot(x4, y4, 'b--', markersize=1)
+    plt.plot(bound4_1, y4, 'k')
+    plt.plot(bound4_2, y4, 'k')
+    # x, y = get_spline(0,3,0,3,np.pi/2,np.pi/9)
+    # x1,y1 = get_spline(3,6,3,0,np.pi/9,np.pi/5)
+    # plt.plot(x,y)
+    # plt.plot(x1,y1)
+    plt.show()
