d1df76b53ddcf86babb7c86c115395dc08ab5d7b
Dikshuy <dikshant1212hsr@gmail.com>
2022-12-22 19:08:17 +0900

    resolved the frenet to cartesian frame conversion bug

--- route.py
+++ route.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from environment import *
@@ -6,15 +7,19 @@ from traffic_msgs import *
 from nav_msgs import *
 from geometry_msgs import *
 
-def xy_to_frenet(x1, y1, current_waypt, destination_waypt):
+def xy_to_frenet(x1, y1, current_waypoint, destination_waypoint):
     # return frenet coordinates from catersian coordinates
     pose_arr = []
-    x_g, y_g = get_spline(0,3,0,3,np.pi/2,np.pi/4)
-    lane_route = [x_g, y_g]
+    # x_g, y_g = get_spline(current_waypoint[0],destination_waypoint[0],current_waypoint[1],destination_waypoint[1],np.pi/2,np.pi/4)
+    lane_route = []
+    for i in range(len(x1)):
+        lane_route.append([x1[i], y1[i]])
+    
     for i in range(len(lane_route)-1):
         point = Point(lane_route[i][0], lane_route[i][1])
         # replace this by actual yaw of the vehicle maybe
-        yaw = np.arctan((lane_route[i+1][1]-lane_route[i][1])/(lane_route[i+1][0]-lane_route[i][0]))
+        yaw = math.atan2((lane_route[i+1][1]-lane_route[i][1]),(lane_route[i+1][0]-lane_route[i][0]))
+        # print(yaw)
         out = quaternion_from_euler(0,0,yaw)
         quat = Quaternion(out[0], out[1], out[2], out[3])
         poses = PoseStamped(std_msgs.Header(), Pose(point, quat))
@@ -37,7 +42,7 @@ def frenet_to_xy_test(s_arr, d_arr, lane_line_list, lane_s_map):
     # return cartesian coordinates from frenet coordinates
     x_arr = []
     y_arr = []
-    for i in range(len(s_arr)):
+    for i in range(1,len(s_arr)):
         x, y, _ = get_xy(s_arr[i], d_arr[i], lane_line_list, lane_s_map)
         x_arr.append(x)
         y_arr.append(y)
@@ -59,15 +64,15 @@ lane_center4, id4 = register_lane(x4, y4, count+3)
 # lane_info = lane_publisher.publish_all_lanes()  # get lanes info without carla
 
 x, y = get_spline(0,3,0,3,np.pi/2,np.pi/4)
-# x_,y_ = get_spline(3,6,3,0,np.pi/9,np.pi/5)
-# x = x + x_
-# y = y + y_
+x_,y_ = get_spline(3,6,3,0,np.pi/2,np.pi/4)
+x = x + x_
+y = y + y_
 
-current_waypoint = [1,1]
-destination_waypoint = [2,2]
+current_waypoint = [3.04, 3.05]
+destination_waypoint = [8.45, 2.875]
 s, d, lane_line, s_map = xy_to_frenet(x, y, current_waypoint, destination_waypoint)
 x_test, y_test = frenet_to_xy_test(s, d, lane_line, s_map)
-# plt.plot(x, y)
-# plt.plot(d, s)
-plt.plot(x_test, y_test)
+plt.plot(x, y)
+plt.plot(d, s)
+plt.plot(x_test, y_test, 'r*')
 plt.show()
