4824e10869de5f2edd3053ac4889a0f6972e6234
Dikshuy <dikshant1212hsr@gmail.com>
2023-01-16 11:07:58 +0900

    added route for every vehicle

--- src/lane_info.py
+++ src/lane_info.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     #  straight lanes
     x1, y1 = get_lanes([-6,-1],[-6,-8])     # down right
     x2, y2 = get_lanes([-6,8], [-6,1])      # down left
-    x3, y3 = get_lanes([-1,0], [-5,0])      # down center
+    x3, y3 = get_lanes([-5,0], [-1,0])      # down center
     x4, y4 = get_lanes([0,5], [0,1])        # center left
     x5, y5 = get_lanes([0,-1], [0,-5])       # center right
     x6, y6 = get_lanes([1,0], [5.5,0])      # up center
@@ -54,6 +54,7 @@ if __name__ == '__main__':
     x9, y9 = get_spline([-6,-1], [-5,0], np.pi/2,0)
     x10, y10 = get_spline([-6,-1], [-6,1], np.pi/2, -np.pi/2)
     x11, y11 = get_spline([-6,1], [-5,0], -np.pi/2, 0)
+
     # X intersection
     x12, y12 = get_spline([-1,0], [0,1], 0, np.pi/2)
     x13, y13 = get_spline([-1,0], [0,-1], 0, -np.pi/2)
@@ -66,6 +67,27 @@ if __name__ == '__main__':
     x18, y18 = get_spline([5.5,0], [6.5, 0.5], 0, np.pi/4)
     x19, y19 = get_spline([5.5,0], [6.5,-0.5], 0, -np.pi/4)
     x20, y20 = get_spline([6.5,0.5], [6.5,-0.5], -(np.pi/4+np.pi/2), -np.pi/4)
+
+    # vehicle trajectories
+    # car 1 path
+    x_car_1 = np.hstack((x3, x14, x6, x19, x7))
+    y_car_1 = np.hstack((y3, y14, y6, y19, y7))   
+
+    # car 2 path 
+    x_car_2 = np.hstack((x7[::-1], x19[::-1], x6[::-1], x14[::-1], x3[::-1]))
+    y_car_2 = np.hstack((y7[::-1], y19[::-1], y6[::-1], y14[::-1], y3[::-1]))
+
+    # car 3 path
+    x_car_3 = np.hstack((x4, x17, x6, x18, x8))
+    y_car_3 = np.hstack((y4, y17, y6, y18, y8))
+
+    # car 4 path
+    x_car_4 = np.hstack((x1[::-1], x9, x3, x12, x4[::-1]))
+    y_car_4 = np.hstack((y1[::-1], y9, y3, y12, y4[::-1]))
+
+    # car 5 path
+    x_car_5 = np.hstack((x5[::-1], x13[::-1], x3[::-1], x11[::-1], x2[::-1]))
+    y_car_5 = np.hstack((y5[::-1], y13[::-1], y3[::-1], y11[::-1], y2[::-1]))
 
     # plotting
     plt.plot(x1, y1, 'k')
@@ -89,5 +111,11 @@ if __name__ == '__main__':
     plt.plot(x18, y18, 'b--')
     plt.plot(x19, y19, 'b--')
     plt.plot(x20, y20, 'b--')
+ 
+    plt.plot(x_car_1, y_car_1)
+    plt.plot(x_car_2, y_car_2)
+    plt.plot(x_car_3, y_car_3)
+    plt.plot(x_car_4, y_car_4)
+    plt.plot(x_car_5, y_car_5)
 
     plt.show()
