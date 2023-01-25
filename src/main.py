848333b2fab93cd86c21234879e57e2e42674d16
dikshant-honda <dikshant_s@jp.honda>
2023-01-25 17:50:56 +0900

    added PI controller

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import rospy
 import math
+import time
 import numpy as np
 from std_msgs.msg import Header
 from nav_msgs.msg import Odometry, Path
@@ -12,6 +13,41 @@ from geometry_utils import *
 from lane_info import *
 from collision_predictor.msg import Environment, VehicleState
 import message_filters
+
+class PI:
+    def __init__(self, P=0.2, I=0.1, current_time=None):
+        self.Kp = P
+        self.Ki = I
+
+        self.sample_time = 0.01
+        self.current_time = current_time if current_time is not None else time.time()
+        self.last_time = self.current_time
+
+        self.clear()
+
+    def clear(self):
+        self.SetPoint = 0.0
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.last_error = 0.0
+
+        self.output = 0.0
+
+    def update(self, feedback_value, current_time=None):
+        error = self.SetPoint - feedback_value
+
+        self.current_time = current_time if current_time is not None else time.time()
+        delta_time = self.current_time - self.last_time
+        delta_error = error - self.last_error
+
+        if delta_time >= self.sample_time:
+            self.PTerm = self.Kp*delta_error
+            self.ITerm += error*delta_time
+
+            self.last_time = self.current_time
+            self.last_error = error
+
+            self.output = self.PTerm + self.Ki * self.ITerm 
 
 class Subscriber:
     def __init__(self):
