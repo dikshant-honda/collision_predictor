49a008f1c15df3a9e69049fd9501704db2c6f9cb
dikshant-honda <dikshant_s@jp.honda>
2023-01-27 14:28:46 +0900

    open file bug resolved

--- src/plotter.py
+++ src/plotter.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3
 
+import os
+
 def plotter(car):
+    open_path = "/home/dikshant/catkin_ws/src/collision_predictor/src"
     file_name = "traj_"+car+".txt"
-    file = open(file_name, "r+")
+    file = open(os.path.join(open_path, file_name), "r+")
 
     lines = file.read().split(',')
 
