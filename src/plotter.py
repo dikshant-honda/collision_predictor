1f4d490074bbd89d7c74dc1e012d276bd7f97ba1
dikshant-honda <dikshant_s@jp.honda>
2023-01-27 11:45:07 +0900

    created plotter function for referencing

--- src/plotter.py
+++ src/plotter.py
@@ -1,17 +1,18 @@
-import matplotlib.pyplot as plt
+#! /usr/bin/env python3
 
-file = open("traj_car_1.txt", "r+")
+def plotter(car):
+    file_name = "traj_"+car+".txt"
+    file = open(file_name, "r+")
 
-lines = file.read().split(',')
+    lines = file.read().split(',')
 
-x, y = [], []
-for line in lines:  
-    print(line)  
-    points = line.split()
+    x, y = [], []
 
-for i in range(0, len(points), 2):
-    x.append(float(points[i]))
-    y.append(float(points[i+1]))
+    for line in lines: 
+        points = line.split()
 
-plt.plot(x,y)
-plt.show()
+    for i in range(0, len(points), 2):
+        x.append(float(points[i]))
+        y.append(float(points[i+1]))
+
+    return x, y
