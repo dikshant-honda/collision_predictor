5e6232655aa5367e99f7c77a07ea8dfb9b88ea8e
dikshant-honda <dikshant_s@jp.honda>
2023-02-06 16:20:05 +0900

    tested movement of the custom bot

--- src/move.py
+++ src/move.py
@@ -13,30 +13,14 @@ def update_pos(pub, lin_vel, ang_vel):
 # main function
 if __name__ == '__main__':
     rospy.init_node('move_robot_node')
-    pub1 = rospy.Publisher('/tb3_1/cmd_vel', Twist, queue_size=10)
-    pub2 = rospy.Publisher('/tb3_2/cmd_vel', Twist, queue_size=10)
-    pub3 = rospy.Publisher('/tb3_3/cmd_vel', Twist, queue_size=10)
-    pub4 = rospy.Publisher('/tb3_4/cmd_vel', Twist, queue_size=10)
-    pub5 = rospy.Publisher('/tb3_5/cmd_vel', Twist, queue_size=10)
+    pub = rospy.Publisher('/r2d2_diff_drive_controller/cmd_vel', Twist, queue_size=10)
 
     rate = rospy.Rate(2)
 
     # speed of the turtlebots
-    lin_vel_1 = 0.2
-    ang_vel_1 = 0.0
-    lin_vel_2 = 0.1
-    ang_vel_2 = 0.0
-    lin_vel_3 = 0.1
-    ang_vel_3 = 0.0
-    lin_vel_4 = 0.1
-    ang_vel_4 = 0.0
-    lin_vel_5 = 0.1
-    ang_vel_5 = 0.0
+    lin_vel = 5
+    ang_vel = 0.0
 
     while not rospy.is_shutdown():
-        update_pos(pub1, lin_vel_1, ang_vel_1)
-        update_pos(pub2, lin_vel_2, ang_vel_2)
-        update_pos(pub3, lin_vel_3, ang_vel_3)
-        update_pos(pub4, lin_vel_4, ang_vel_4)
-        update_pos(pub5, lin_vel_5, ang_vel_5)
+        update_pos(pub, lin_vel, ang_vel)
         rate.sleep()
