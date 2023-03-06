1c132688cc0546a17960809268cb0d7e7902fbd2
dikshant-honda <dikshant_s@jp.honda>
2023-03-06 10:58:56 +0900

    lane configurations corrected now

--- src/lane_info_2.py
+++ src/lane_info_2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from geometry_msgs.msg import Point
 import dubins
 
-def get_dubins(start, end, theta0, theta1, step_size = 0.001):
+def get_dubins(start, end, theta0, theta1, step_size = 0.01):
 	q0 = (start[0], start[1], theta0)
 	q1 = (end[0], end[1], theta1)
 
@@ -26,7 +26,7 @@ def get_dubins(start, end, theta0, theta1, step_size = 0.001):
 	
 	return x, y, yaw
 
-def get_straight_dubins(start, end, theta0, theta1, step_size = 0.001):
+def get_straight_dubins(start, end, theta0, theta1, step_size = 0.01):
 	q0 = (start[0], start[1], theta0)
 	q1 = (end[0], end[1], theta1)
 
@@ -97,19 +97,6 @@ def get_linking_route(turning_route):
 		merging_route = lane_3
 	return merging_route
 
-'''
-gazebo coordinate system:
-
-	o ----------------> Y
-	|
-	|
-	|
-	|
-	|
-	v
-	X
-'''
-
 # straight lines information
 x1, y1, yaw1 = get_straight_dubins([13, -0.9], [2.5, -0.9], np.pi, np.pi)     		# down
 x2, y2, yaw2 = get_straight_dubins([2.5, 0.9], [13, 0.9], 0, 0)             		# down 
@@ -140,27 +127,6 @@ x18, y18, yaw18 = get_dubins([0.9, 2.5], [2.5, 0.9], -np.pi/2, 0)					# right to
 x19, y19, yaw19 = get_dubins([0.9, 2.5], [0.9, -2.5], -np.pi/2, -np.pi/2)			# right to left
 x20, y20, yaw20 = get_dubins([0.9, 2.5], [-2.5, -0.9], -np.pi/2, np.pi)				# right to up
 
-# possible car routes // not required anymore
-# from down
-down_to_left = [np.hstack((x1, x9, x6)), np.hstack((y1, y9, y6)), np.hstack((yaw1, yaw9, yaw6))]
-down_to_up = [np.hstack((x1, x10, x3)), np.hstack((y1, y10, y3)), np.hstack((yaw1, yaw10, yaw3))]
-down_to_right = [np.hstack((x1, x11, x7)), np.hstack((y1, y11, y7)), np.hstack((yaw1, yaw11, yaw7))]
-
-# from left
-left_to_up = [np.hstack((x5, x12, x3)), np.hstack((y5, y12, y3)), np.hstack((yaw5, yaw12, yaw3))]
-left_to_right = [np.hstack((x5, x13, x7)), np.hstack((y5, y13, y7)), np.hstack((yaw5, yaw13, yaw7))]
-left_to_down = [np.hstack((x5, x14, x2)), np.hstack((y5, y14, y2)), np.hstack((yaw5, yaw14, yaw2))]
-
-# from up
-up_to_right = [np.hstack((x4, x15, x7)), np.hstack((y4, y15, y7)), np.hstack((yaw4, yaw15, yaw7))]
-up_to_down = [np.hstack((x4, x16, x2)), np.hstack((y4, y16, y2)), np.hstack((yaw4, yaw16, yaw2))]
-up_to_left = [np.hstack((x4, x17, x6)), np.hstack((y4, y17, y6)), np.hstack((yaw4, yaw17, yaw6))]
-
-# from right
-right_to_down = [np.hstack((x8, x18, x2)), np.hstack((y8, y18, y2)), np.hstack((yaw8, yaw18, yaw2))]
-right_to_left = [np.hstack((x8, x19, x6)), np.hstack((y8, y19, y6)), np.hstack((yaw8, yaw19, yaw6))]
-right_to_up = [np.hstack((x8, x20, x3)), np.hstack((y8, y20, y3)), np.hstack((yaw8, yaw20, yaw3))]
-
 # lane information
 lane_1 = [arr_to_point(x1, y1), yaw1]
 lane_2 = [arr_to_point(x2, y2), yaw2]
@@ -183,27 +149,24 @@ lane_18 = [arr_to_point(x18, y18), yaw18]
 lane_19 = [arr_to_point(x19, y19), yaw19]
 lane_20 = [arr_to_point(x20, y20), yaw20]
 
-# plotting
-# plt.plot(y1, -x1, 'g')
-# plt.plot(y2, -x2, 'g')
-# plt.plot(y3, -x3, 'g')
-# plt.plot(y4, -x4, 'g')
-# plt.plot(y5, -x5, 'g')
-# plt.plot(y6, -x6, 'g')
-# plt.plot(y7, -x7, 'g')
-# plt.plot(y8, -x8, 'g')
 
-# plt.plot(y9, -x9, 'r--')
-# plt.plot(y10, -x10, 'r--')
-# plt.plot(y11, -x11, 'r--')
-# plt.plot(y12, -x12, 'b--')
-# plt.plot(y13, -x13, 'b--')
-# plt.plot(y14, -x14, 'b--')
-# plt.plot(y15, -x15, 'b--')
-# plt.plot(y16, -x16, 'b--')
-# plt.plot(y17, -x17, 'b--')
-# plt.plot(y18, -x18, 'b--')
-# plt.plot(y19, -x19, 'b--')
-# plt.plot(y20, -x20, 'b--')
+# possible car routes // not required anymore
+# from down
+down_to_left = [np.hstack((x1, x9, x6)), np.hstack((y1, y9, y6)), np.hstack((yaw1, yaw9, yaw6))]
+down_to_up = [np.hstack((x1, x10, x3)), np.hstack((y1, y10, y3)), np.hstack((yaw1, yaw10, yaw3))]
+down_to_right = [np.hstack((x1, x11, x7)), np.hstack((y1, y11, y7)), np.hstack((yaw1, yaw11, yaw7))]
 
-# plt.show()
+# from left
+left_to_up = [np.hstack((x5, x12, x3)), np.hstack((y5, y12, y3)), np.hstack((yaw5, yaw12, yaw3))]
+left_to_right = [np.hstack((x5, x13, x7)), np.hstack((y5, y13, y7)), np.hstack((yaw5, yaw13, yaw7))]
+left_to_down = [np.hstack((x5, x14, x2)), np.hstack((y5, y14, y2)), np.hstack((yaw5, yaw14, yaw2))]
+
+# from up
+up_to_right = [np.hstack((x4, x15, x7)), np.hstack((y4, y15, y7)), np.hstack((yaw4, yaw15, yaw7))]
+up_to_down = [np.hstack((x4, x16, x2)), np.hstack((y4, y16, y2)), np.hstack((yaw4, yaw16, yaw2))]
+up_to_left = [np.hstack((x4, x17, x6)), np.hstack((y4, y17, y6)), np.hstack((yaw4, yaw17, yaw6))]
+
+# from right
+right_to_down = [np.hstack((x8, x18, x2)), np.hstack((y8, y18, y2)), np.hstack((yaw8, yaw18, yaw2))]
+right_to_left = [np.hstack((x8, x19, x6)), np.hstack((y8, y19, y6)), np.hstack((yaw8, yaw19, yaw6))]
+right_to_up = [np.hstack((x8, x20, x3)), np.hstack((y8, y20, y3)), np.hstack((yaw8, yaw20, yaw3))]
