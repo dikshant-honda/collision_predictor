e19ef3afc44e6bb071cdd4db7879076673554cea
dikshant-honda <dikshant_s@jp.honda>
2023-03-15 13:31:00 +0900

    helper files added

---
+++ helper/frenet.py
@@ -0,0 +1,350 @@
+import numpy as np
+import math
+import bisect
+from geometry_utils import *
+
+'''
+note: use the get_frenet_with_theta to get the yaw of the vehicle in local frame,
+think about this and find out a way to store the theta of the vehicle
+'''
+def global_to_local(ref_orig, orientation, p):
+    # converting the global frame to local frame for helping frenet transform
+    delta = Point2D(p.x - ref_orig.x, p.y - ref_orig.y)
+
+    s = math.sin(-orientation)
+    c = math.cos(-orientation)
+
+    out = Point2D(delta.x * c - delta.y * s,
+    delta.x * s + delta.y * c)
+
+    return out
+
+def local_to_global(center, theta, p):
+    # converting the local frame to global frame for bringing back to cartesian coordinates
+    s = math.sin(theta)
+    c = math.cos(theta)
+
+    out = Point2D(p.x * c - p.y * s + center.x, p.x * s + p.y * c + center.y)
+
+    return out
+
+def path_to_list(nav_path):
+    # converting ther nav_path message type to list for ease in accessibility
+    # generating s_map from the start point till end point for transforms
+    path_list = []
+    distance_acum = 0.0
+    s_map = []
+    prev_p = None
+    for pose in nav_path.poses:
+        x = pose.pose.position.x
+        y = pose.pose.position.y
+        path_list.append(Point2D(x, y))
+        if prev_p != None:
+            distance_acum += distance(prev_p.x, prev_p.y, x, y)
+        s_map.append(distance_acum)
+        prev_p = Point2D(x, y)
+    return path_list, s_map
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+
+def closest_point_ind(path, x, y):
+    # finding the closest index on path from point(x,y)
+    index = 0
+    closest_index = 0
+    min_dist = 10000.0
+    for p in path:
+        dist = distance(p.x, p.y, x, y)
+        if dist < min_dist:
+            min_dist = dist
+            closest_index = index
+        index += 1
+    return closest_index
+
+# Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+def get_frenet(x, y, path, s_map):
+    if path == None:
+        print("Empty map. Cannot return Frenet coordinates")
+        return 0.0, 0.0, False
+
+    ind_closest = closest_point_ind(path, x, y)
+
+    # Determine the indices of the 2 closest points
+    if ind_closest < len(path):
+        # Check if we are at the end of the segment
+        if ind_closest == len(path) - 1:
+            use_previous = True
+        elif ind_closest == 0:
+            use_previous = False
+        else:
+            dist_prev = distance(path[ind_closest-1].x, path[ind_closest-1].y, x, y)
+            dist_next = distance(path[ind_closest+1].x, path[ind_closest+1].y, x, y)
+
+            if dist_prev <= dist_next:
+                use_previous = True
+            else:
+                use_previous = False
+
+        # Get the 2 points
+        if use_previous:
+            p1 = Point2D(path[ind_closest - 1].x, path[ind_closest - 1].y)
+            p2 = Point2D(path[ind_closest].x, path[ind_closest].y)
+            prev_idx = ind_closest - 1
+        else:
+            p1 = Point2D(path[ind_closest].x, path[ind_closest].y)
+            p2 = Point2D(path[ind_closest + 1].x, path[ind_closest + 1].y)
+            prev_idx = ind_closest
+
+        # Get the point in the local coordinate with center p1
+        theta = math.atan2(p2.y - p1.y, p2.x - p1.x)
+        local_p = global_to_local(p1, theta, Point2D(x,y))
+
+        # Get the coordinates in the Frenet frame
+        p_s = s_map[prev_idx] + local_p.x
+        p_d = local_p.y
+
+    else:
+        print("Incorrect index")
+        return 0.0, 0.0, False
+
+    return p_s, p_d, True
+
+# adding on theta to get the yaw of the vehicle
+def get_frenet_with_theta(x, y, path, s_map):
+    if path == None:
+        print("Empty map. Cannot return Frenet coordinates")
+        return 0.0, 0.0, 0.0, False
+
+    ind_closest = closest_point_ind(path, x, y)
+
+    # Determine the indices of the 2 closest points
+    if ind_closest < len(path):
+        # Check if we are at the end of the segment
+        if ind_closest == len(path) - 1:
+            use_previous = True
+        elif ind_closest == 0:
+            use_previous = False
+        else:
+            dist_prev = distance(path[ind_closest-1].x, path[ind_closest-1].y, x, y)
+            dist_next = distance(path[ind_closest+1].x, path[ind_closest+1].y, x, y)
+
+            if dist_prev <= dist_next:
+                use_previous = True
+            else:
+                use_previous = False
+
+        # Get the 2 points
+        if use_previous:
+            p1 = Point2D(path[ind_closest - 1].x, path[ind_closest - 1].y)
+            p2 = Point2D(path[ind_closest].x, path[ind_closest].y)
+            prev_idx = ind_closest - 1
+        else:
+            p1 = Point2D(path[ind_closest].x, path[ind_closest].y)
+            p2 = Point2D(path[ind_closest + 1].x, path[ind_closest + 1].y)
+            prev_idx = ind_closest
+
+        # Get the point in the local coordinate with center p1
+        theta = math.atan2(p2.y - p1.y, p2.x - p1.x)
+        local_p = global_to_local(p1, theta, Point2D(x,y))
+
+        # Get the coordinates in the Frenet frame
+        p_s = s_map[prev_idx] + local_p.x
+        p_d = local_p.y
+
+    else:
+        print("Incorrect index")
+        return 0.0, 0.0, 0.0, False
+
+    return p_s, p_d, theta, True
+
+# Transform from Frenet s,d coordinates to Cartesian x,y
+def get_xy(s, d, path, s_map):
+
+    if path == None or s_map == None:
+        print("Empty path. Cannot compute Cartesian coordinates")
+        return 0.0, 0.0, False
+
+    # If the value is out of the actual path send a warning
+    if s < 0.0 or s > s_map[-1]:
+        if s < 0.0:
+            prev_point = 0
+        else:
+            prev_point = len(s_map) -2
+    else:
+        # Find the previous point
+        idx = bisect.bisect_left(s_map, s)
+        prev_point = idx - 1
+
+    p1 = path[prev_point]
+    p2 = path[prev_point + 1]
+
+    # Transform from local to global
+    theta = math.atan2(p2.y - p1.y, p2.x - p1.x)
+    p_xy = local_to_global(p1, theta, Point2D(s - s_map[prev_point], d))
+
+    return p_xy.x, p_xy.y, True
+
+def dist_to_line(p1, p2, p):
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+    p = np.array(p)
+    d = np.cross(p2-p1,p-p1)/np.linalg.norm(p2-p1)
+    return abs(d)
+
+'''
+past functions converted from cpp, not required anymore!
+'''
+path_m = Point2D(0,0)
+path_s_m = []
+path_theta_m = []
+
+def CreateSPath(spec_origin, origin):
+    if len(path_m) < 2: return
+
+    path_s_m = np.resize(path_s_m, len(path_m))
+    path_theta_m = np.resize(path_theta_m, len(path_m))
+
+    if not spec_origin:
+        path_s_m[0] = 0
+        for i in range(1, len(path_s_m)):
+            path_s_m[i] = distance(path_m[i], path_m[i-1]) + path_s_m[i-1]
+            dir_vec = path_m[i] - path_m[i-1]
+            path_theta_m[i-1] = math.atan2(dir_vec.y, dir_vec.x)
+    
+        path_theta_m[len(path_theta_m)-1] = path_theta_m[len(path_theta_m)-2]
+    else:
+        ind_closest = findClosestIndex2D(path_m, origin)
+        path_s_m[ind_closest] = 0
+
+        for i in range(ind_closest-1, -1, -1):
+            path_s_m[i] = path_s_m[i+1] - distance(path_m[i], path_m[i+1])
+            dir_vec = path_m[i+1] - path_m[i]
+            path_theta_m[i] = math.atan2(dir_vec.y, dir_vec.x)
+
+        for i in range(ind_closest + 1, len(path_m)):
+            path_s_m[i] = distance(path_m[i], path_m[i-1]) + path_s_m[i-1]
+            dir_vec = path_m[i] - path_m[i-1]
+            path_theta_m[i-1] = math.atan2(dir_vec.y, dir_vec.x)
+        path_theta_m[len(path_theta_m)-1] = path_theta_m[len(path_theta_m)-2]
+
+def SetPath(path, spec_origin, origin):
+    if len(path) < 2:
+        path_m.clear() 
+        path_s_m.clear()
+        print("Frenet path needs at least 2 points")
+        return
+    path_m = path
+    CreateSPath(spec_origin, origin)
+
+def findClosestIndex2DWithDirection(point_input, theta):
+    min_distance = np.inf
+    ind_closest = -1
+    for i in range(0, len(path_m)):
+        aux_angle = math.atan2(np.sin(path_theta_m[i]-theta), np.cos(path_theta_m[i]-theta))
+        if aux_angle < -3*np.pi/4 or aux_angle > 3*np.pi/4:
+            continue
+        distance_val = distance(point_input, path_m[i])
+        if distance_val < min_distance:
+            min_distance = distance_val
+            ind_closest = i
+    
+    return ind_closest
+
+def ToFrenet(p_xy, p_sd, road_dir, closest):
+    if len(path_m) < 2:
+        print("Path needs at least 2 points. Cannot compute Frenet")
+        p_sd.s = -np.inf
+        p_sd.d = -np.inf
+        return False
+    
+    if closest == -1:
+        closest = findClosestIndex2D(path_m, p_xy)
+    
+    closest_ind = 0
+    prev_idx = 0
+    if closest < 0:
+        print("Wrong closest index. Cannot compute Frenet")
+        p_sd.s = -np.inf
+        p_sd.d = -np.inf
+        return False
+    else:
+        closest_ind = closest
+
+    use_previous = False
+
+    if closest_ind < len(path_m):
+        if closest_ind == len(path_m)-1:
+            use_previous = True
+        elif closest == 0:
+            use_previous = False
+        else:
+            dist_prev = distance(path_m[closest_ind - 1], p_xy)
+            dist_next = distance(path_m[closest_ind + 1], p_xy)
+            if dist_prev <= dist_next:
+                use_previous = True
+            else:
+                use_previous = False
+        
+        p1 = Point2D()
+        p2 = Point2D()
+        if use_previous:
+            p1.x = path_m[closest_ind - 1].x
+            p1.y = path_m[closest_ind - 1].y
+            p2.x = path_m[closest_ind].x
+            p2.y = path_m[closest_ind].y
+            prev_idx = closest_ind - 1
+        else:
+            p1.x = path_m[closest_ind].x
+            p1.y = path_m[closest_ind].y
+            p2.x = path_m[closest_ind + 1].x
+            p2.y = path_m[closest_ind + 1].y
+            prev_idx = closest_ind
+        
+        road_dir = math.atan2(p2.y - p1.y, p2.x - p1.x)
+        local_p = globalToLocal(p1, road_dir, p_xy)
+
+        p_sd.s = path_s_m[prev_idx] + local_p.x
+        p_sd.d = local_p.y
+    else:
+        print("incorrect index")
+        p_sd.s = -np.inf
+        p_sd.d = -np.inf
+        road_dir = 100
+        return False
+    return True
+
+def ToFrenetDirectional(p_xy, theta, p_sd, road_dir):
+    closest = findClosestIndex2DWithDirection(p_xy, theta)
+    if closest == -1:
+        print("Can't find index in frenet directional")
+        return False
+    return ToFrenet(p_xy, p_sd, road_dir, closest)
+
+def ToCartesian(p_sd, p_xy, road_dir):
+    if len(path_m) < 2:
+        print("Path needs at least 2 points. Cannot compute Frenet")
+        p_xy.x = -np.inf
+        p_xy.y = -np.inf
+        return False
+
+    prev_point_ind = 0
+
+    if p_sd.s <= path_s_m[0] or p_sd.s >= path_s_m[-1]:
+        if p_sd.s < path_s_m[0]:
+            prev_point_ind = 0
+        else:
+            prev_point_ind = len(path_s_m) - 2
+    else:
+        it = bisect.bisect_left(path_s_m, p_sd.s)
+        prev_point_ind = it - path_s_m[0] - 1
+    
+    p1 = Point2D()
+    p2 = Point2D()
+    p1.x = path_m[prev_point_ind].x
+    p1.y = path_m[prev_point_ind].y
+    p2.x = path_m[prev_point_ind+1].x
+    p2.y = path_m[prev_point_ind+1].y
+
+    road_dir = math.atan2(p2.y - p1.y, p2.x - p1.x)
+    p_xy = localToGlobal(p1, road_dir,Point2D(p_sd.s - path_s_m[prev_point_ind], p_sd.d))
+    return True
---
+++ helper/update_pos.py
@@ -0,0 +1,129 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from environment import *
+from lane_publisher import *
+from py_msgs.traffic_msgs import *
+from py_msgs.nav_msgs import *
+from py_msgs.geometry_msgs import *
+
+'''
+note:
+1. reduce "d" more gradually, store the past d_vals, compute d_avg from there to get the
+average deviation from the center line
+2. assume that the driver will tend to come back to the center lane so reduce by some 
+factor of d_avg after few time steps
+
+TO DO:
+1. change the s-path by the entire trajectory in FOV and get lane route from there
+2. update the predict trajectory vehicles, offset from center(d) from the idea mentioned above
+3. change the s=ut dynamics and try to get the vehicle towards the lane
+'''
+
+def get_lane_and_s_map(x1, y1):
+    pose_arr = []
+    # x_g, y_g = get_spline(current_waypoint[0],destination_waypoint[0],current_waypoint[1],destination_waypoint[1],np.pi/2,np.pi/4)
+    lane_route = []
+    for i in range(len(x1)):
+        lane_route.append([x1[i], y1[i]])
+    
+    for i in range(len(lane_route)-1):
+        point = Point(lane_route[i][0], lane_route[i][1])
+        # replace this by actual yaw of the vehicle maybe
+        yaw = math.atan2((lane_route[i+1][1]-lane_route[i][1]),(lane_route[i+1][0]-lane_route[i][0]))
+        out = quaternion_from_euler(0,0,yaw)
+        quat = Quaternion(out[0], out[1], out[2], out[3])
+        poses = PoseStamped(std_msgs.Header(), Pose(point, quat))
+        pose_arr.append(poses)
+    # adding the last point
+    pose_arr.append(PoseStamped(std_msgs.Header(), Pose(Point(lane_route[-1][0], lane_route[-1][1]), Quaternion(out[0], out[1], out[2], out[3]))))
+    path_route = Path(std_msgs.Header(), pose_arr)
+    lane_line_list, lane_s_map = path_to_list(path_route)
+
+    return lane_line_list, lane_s_map
+
+def PredictTrajectoryVehicles(init_x, init_y, path, s_map, v):    # msg_vehicles -> traffic_msg/PredictionArray
+    s, d, _ = get_frenet(init_x, init_y, path, s_map)
+    future_x = []
+    future_y = []
+    for t in range(np_m):
+        if t < interp_back_path:
+            d_val = d - ((t*d) / interp_back_path)
+            new_x, new_y, _ = get_xy(s+v*dt_m*t, d_val, path, s_map)
+        else:
+            new_x, new_y, _ = get_xy(s+v*dt_m*t, 0, path, s_map)
+        future_x.append(new_x)
+        future_y.append(new_y)
+    return future_x, future_y
+
+# for time being, i am updating the next position from the future trajectory point 
+# directly. in real world, we will get it from pose after the detection part
+# collect the info from vehicle[i].twist.twist.linear._
+def move(x, y, v, dt_m, path):
+    # find the closest index from the curve and compute theta between those points
+    # shift the vehicle along that direction to get the modified points
+    # in real world, we will get it from our detection algorithm
+
+    ind_closest = closest_point_ind(path, x, y)
+    # Determine the indices of the 2 closest points
+    if ind_closest < len(path):
+        # Check if we are at the end of the segment
+        if ind_closest == len(path) - 1:
+            use_previous = True
+        elif ind_closest == 0:
+            use_previous = False
+        else:
+            dist_prev = distance(path[ind_closest-1].x, path[ind_closest-1].y, x, y)
+            dist_next = distance(path[ind_closest+1].x, path[ind_closest+1].y, x, y)
+
+            if dist_prev <= dist_next:
+                use_previous = True
+            else:
+                use_previous = False
+
+        # Get the 2 points
+        if use_previous:
+            p1 = Point2D(path[ind_closest - 1].x, path[ind_closest - 1].y)
+            p2 = Point2D(path[ind_closest].x, path[ind_closest].y)
+        else:
+            p1 = Point2D(path[ind_closest].x, path[ind_closest].y)
+            p2 = Point2D(path[ind_closest + 1].x, path[ind_closest + 1].y)
+
+        # Get the point in the local coordinate with center p1
+        theta = math.atan2(p2.y - p1.y, p2.x - p1.x)
+    new_x = x + v*np.cos(theta)*dt_m
+    new_y = y + v*np.sin(theta)*dt_m
+
+    return [new_x, new_y]
+
+interp_back_path = 5
+plan_t_m = 1
+dt_m = 0.1
+np_m = int(plan_t_m/dt_m)
+# velocity obtained from vehicles.twist.twist.linear.x
+v = 0.5                   # change later
+
+# # main function
+# if __name__ == '__main__':
+#     # path
+#     x, y = get_spline(0,3,0,3,np.pi/2,np.pi/4)
+#     x_,y_ = get_spline(3,6,3,0,np.pi/2,np.pi/4)
+#     x = x + x_
+#     y = y + y_
+#     plt.plot(x, y, 'g')
+
+#     current_waypoint = [3.04, 3.05]
+
+#     # replace by collision check
+#     horizon = 0
+#     while horizon < 114:
+#         lane_line_list, lane_s_map = get_lane_and_s_map(x, y)
+#         future_x, future_y = PredictTrajectoryVehicles(current_waypoint[0], current_waypoint[1], lane_line_list, lane_s_map)
+#         current_waypoint = move(current_waypoint[0], current_waypoint[1], v, dt_m, lane_line_list)
+#         # update these waypoints as ros messages -> geometry_msgs.pose.position
+#         # later provide this information on ros traffic messages
+#         horizon += 1
+#         plt.plot(future_x, future_y, 'r--')
+#         plt.plot(current_waypoint[0], current_waypoint[1], 'b*')
+#         plt.pause(0.2)
+    # plt.show()
