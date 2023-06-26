57875f588050240c9e3bd0dc59952e09534fbdcf
dikshant-honda <dikshant_s@jp.honda>
2023-06-26 10:00:26 +0900

    linking the package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+d = generate_distutils_setup(
+    packages=['collision_prediction'],
+    package_dir={'': 'src'}
+)
+setup(**d)
