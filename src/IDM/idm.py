0f19b645c85aed0af3ba2ce6ec62e30870540d27
dikshant-honda <dikshant_s@jp.honda>
2023-07-10 18:43:53 +0900

    test for 2 vehicles

--- src/IDM/idm.py
+++ src/IDM/idm.py
@@ -22,13 +22,13 @@ class IDM:
         return acceleration
 
 
-def predict_trajectory(idm, initial_speed, initial_position, time_horizon, time_step):
+def predict_trajectory(idm, initial_speed, lead_speed, gap, initial_position, time_horizon, time_step):
     speed = initial_speed
     position = initial_position
     trajectory = [(position, speed)]
 
-    for t in range(time_horizon):
-        acceleration = idm.calculate_acceleration(speed, 0, math.inf)  # Assuming no lead vehicle
+    for _ in range(time_horizon):
+        acceleration = idm.calculate_acceleration(speed, lead_speed, gap)  # Assuming no lead vehicle
         speed += acceleration * time_step
         position += speed * time_step
         trajectory.append((position, speed))
@@ -36,15 +36,35 @@ def predict_trajectory(idm, initial_speed, initial_position, time_horizon, time_
     return trajectory
 
 
-# Example usage
 idm = IDM()
 initial_speed = 20  # Initial speed of the vehicle
 initial_position = 0  # Initial position of the vehicle
-time_horizon = 12  # Time horizon for trajectory prediction (in seconds)
+time_horizon = 10  # Time horizon for trajectory prediction (in seconds)
 time_step = 0.1  # Time step for trajectory prediction (in seconds)
 
-trajectory = predict_trajectory(idm, initial_speed, initial_position, time_horizon, time_step)
+trajectory = predict_trajectory(idm, initial_speed, 0, math.inf, initial_position, time_horizon, time_step)
+
+ego_vehicle = IDM()
+ego_vehicle_speed = 20
+ego_initial_position = 0
+
+lead_vehicle = IDM()
+lead_vehicle_speed = 30
+lead_initial_position = 10
+
+# initial iteration
+init_gap = lead_initial_position - ego_initial_position
+ego_trajectory = predict_trajectory(ego_vehicle ,ego_vehicle_speed, lead_vehicle_speed, init_gap, ego_initial_position, time_horizon, time_step)
+lead_trajectory = predict_trajectory(lead_vehicle, lead_vehicle_speed, 0, math.inf, lead_initial_position, time_horizon, time_step)
 
 print("Predicted Trajectory:")
-for t, (position, speed) in enumerate(trajectory):
+for t, (position, speed) in enumerate(ego_trajectory):
     print(f"Time: {t * time_step:.1f}s, Position: {position:.2f}m, Speed: {speed:.2f}m/s")
+
+for t, (position, speed) in enumerate(lead_trajectory):
+    print(f"Time: {t * time_step:.1f}s, Position: {position:.2f}m, Speed: {speed:.2f}m/s")
+
+for _ in range(10):
+    gap = lead_trajectory[-1][0] - ego_trajectory[-1][0]
+    ego_trajectory = predict_trajectory(ego_vehicle ,ego_vehicle_speed, lead_vehicle_speed, init_gap, ego_initial_position, time_horizon, time_step)
+    lead_trajectory = predict_trajectory(lead_vehicle, lead_vehicle_speed, 0, math.inf, lead_initial_position, time_horizon, time_step)
