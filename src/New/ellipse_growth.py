df0e1a4e702d6c0692a136387feff5b581bbdfa2
dikshant-honda <dikshant_s@jp.honda>
2023-07-17 13:30:34 +0900

    perfection

--- src/New/ellipse_growth.py
+++ src/New/ellipse_growth.py
@@ -1,5 +1,8 @@
-import numpy as np
+from __future__ import annotations
+
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 center = [0, 0]
 
@@ -8,14 +11,12 @@ b = 2
 
 theta = np.linspace(0, 2 * np.pi, 100)
 
-scale_factor  = 1.1
-
-v = 2
+v = 5
 for t in range(12):
     center[0] = v * t
-
-    x = center[0] + (scale_factor * t * a) * np.cos(theta)
-    y = center[1] + (scale_factor * t * b) * np.sin(theta)
+    scale_factor = 1 + 0.14 * t
+    x = center[0] + (scale_factor * a) * np.cos(theta)
+    y = center[1] + (scale_factor * b) * np.sin(theta)
 
     plt.plot(center[0], center[1], "*")
     plt.plot(x, y)
