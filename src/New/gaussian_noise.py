ea9156a6149f2b77ca05c42cf32014d12b34a788
dikshant-honda <dikshant_s@jp.honda>
2023-07-17 17:10:30 +0900

    added gaussian noise to the position

--- src/New/gaussian_noise.py
+++ src/New/gaussian_noise.py
@@ -2,16 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def multivariate_normal(x, d, mean, covariance):
-    """pdf of the multivariate normal distribution."""
     x_m = x - mean
     return (1. / (np.sqrt((2 * np.pi)**d * np.linalg.det(covariance))) * 
             np.exp(-(np.linalg.solve(covariance, x_m).T.dot(x_m)) / 2))
 
 # Plot bivariate distribution
-def generate_surface(mean, covariance, d):
-    nb_of_x = 50 # grid size
-    xs = np.linspace(-50 , 150, num=nb_of_x)
-    ys = np.linspace(-5 , 5, num=nb_of_x)
+def generate_surface(mean, covariance, d, shift):
+    nb_of_x = 100 # grid size
+    xs = np.linspace(-40 + shift , 40 + shift , num=nb_of_x)
+    ys = np.linspace(-40 , 40 , num=nb_of_x)
     x, y = np.meshgrid(xs, ys) # Generate grid
     pdf = np.zeros((nb_of_x, nb_of_x))
     
@@ -22,25 +21,38 @@ def generate_surface(mean, covariance, d):
                 d, mean, covariance)
     return x, y, pdf 
  
-fig, ax = plt.subplots()
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-# ax.set_aspect('equal')
-ax.axis([-50, 50, -25, 25])
-
+mean = np.matrix([[0], [0.]])
+cov = np.matrix([
+        [4., 0.], 
+        [0., 2.]])
 d = 2  
+x, y, pdf = generate_surface(mean, cov, d, 0)
+
+plt.figure()
+contour = plt.contour(x, y, pdf, levels = 100, cmap = "viridis")
+
+plt.title('Contour plot for positions')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.xlim(-40, 120)
+plt.ylim(-20, 20)
+# plt.axis('equal')
 
 init_pos = np.matrix([[0.], [0.]])
-v = 5
+v = 10
 
-for t in range(10):
+for t in range(1, 10):
     bivariate_mean = np.matrix([[v*t], [0.]])
     bivariate_covariance = np.matrix([
         [4., 0.], 
         [0., 2.]]) 
-    x1, x2, p = generate_surface(
-        bivariate_mean, bivariate_covariance, d)
-    # Plot bivariate distribution
-    con = ax.contourf(x1, x2, p, 33)
-
+    x, y, p = generate_surface(
+        bivariate_mean, bivariate_covariance, d, v*t)
+    
+    contour.remove()
+    contour = plt.contour(x, y, p, levels = 33, cmap = "viridis")
+    
+    plt.draw()
+    plt.pause(0.5)
+ 
 plt.show()
