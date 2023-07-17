d2175c9d064040e6744805a750e958e57552124e
dikshant-honda <dikshant_s@jp.honda>
2023-07-17 11:54:46 +0900

    ellipse size update with moving vehicle ready

--- src/New/ellipse_growth.py
+++ src/New/ellipse_growth.py
@@ -7,23 +7,17 @@ a = 4
 b = 2
 
 theta = np.linspace(0, 2 * np.pi, 100)
-x = center[0] + a * np.cos(theta)
-y = center[1] + b * np.sin(theta)
 
-# plt.plot(x, y)
-
-# plt.axis("equal")
-# plt.show()
-
-scale_factor  = 1.4
+scale_factor  = 1.1
 
 v = 2
 for t in range(12):
-    center[0] += v * t
+    center[0] = v * t
 
-    x = scale_factor * (center[0] + a * np.cos(theta))
-    y = scale_factor * (center[1] + b * np.sin(theta)) 
+    x = center[0] + (scale_factor * t * a) * np.cos(theta)
+    y = center[1] + (scale_factor * t * b) * np.sin(theta)
 
+    plt.plot(center[0], center[1], "*")
     plt.plot(x, y)
     plt.draw()
     plt.axis('equal')
