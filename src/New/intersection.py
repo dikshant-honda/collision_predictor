37ead1e0bd76d74577f83f8ba86b3229d7188cc9
dikshant-honda <dikshant_s@jp.honda>
2023-07-18 17:30:27 +0900

    formating done

--- src/New/intersection.py
+++ src/New/intersection.py
@@ -1,38 +1,42 @@
-from math import sqrt, acos, floor, sin
+from math import acos, floor, sin, sqrt
 
-# Function to return area of intersection
-def intersectionArea(X1, Y1, R1, X2, Y2, R2):
+import numpy as np
 
-    Pi = 3.14
-    
-    # Calculate the euclidean distance
-    # between the two points
+
+def intersectionArea(
+    X1: float,
+    Y1: float,
+    R1: float,
+    X2: float,
+    Y2: float,
+    R2: float,
+) -> float:
+    """
+    calculate the overlap area of two circles
+
+    args: 
+        X1, Y1, R1: (X1, Y1) represents the center of circle1 with radius R1
+        X2, Y2, R2: (X2, Y2) represents the center of circle2 with radius R2
+    """
+
+    np.pi = 3.14
+
     d = sqrt(((X2 - X1) * (X2 - X1)) + ((Y2 - Y1) * (Y2 - Y1)))
 
-    if (d > R1 + R2) :
+    if (d > R1 + R2):
         ans = 0
 
-    elif (d <= (R1 - R2) and R1 >= R2) :
-        ans = floor(Pi * R2 * R2)
+    elif (d <= (R1 - R2) and R1 >= R2):
+        ans = floor(np.pi * R2 * R2)
 
-    elif (d <= (R2 - R1) and R2 >= R1) :
-        ans = floor(Pi * R1 * R1)
+    elif (d <= (R2 - R1) and R2 >= R1):
+        ans = floor(np.pi * R1 * R1)
 
-    else :
+    else:
         alpha = acos(((R1 * R1) + (d * d) - (R2 * R2)) / (2 * R1 * d)) * 2
         beta = acos(((R2 * R2) + (d * d) - (R1 * R1)) / (2 * R2 * d)) * 2
-        
-        a1 = (0.5 * beta * R2 * R2 ) - (0.5 * R2 * R2 * sin(beta))
+
+        a1 = (0.5 * beta * R2 * R2) - (0.5 * R2 * R2 * sin(beta))
         a2 = (0.5 * alpha * R1 * R1) - (0.5 * R1 * R1 * sin(alpha))
         ans = floor(a1 + a2)
-
     return ans
-
-# Driver Code
-if __name__ == "__main__" :
-
-    X1 = 0; Y1 = 0; R1 = 4
-    X2 = 6; Y2 = 0; R2 = 4
-
-    print(intersectionArea(X1, Y1, R1, X2, Y2, R2))
-    
