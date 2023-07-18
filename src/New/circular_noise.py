b82cd5fe750371538274ccdb1ad278f787812f8e
dikshant-honda <dikshant_s@jp.honda>
2023-07-18 17:42:18 +0900

    converted into noise function

--- src/New/circular_noise.py
+++ src/New/circular_noise.py
@@ -1,29 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
-ax.set_aspect('equal')
+# fig, ax = plt.subplots()
+# ax.set_aspect('equal')
 
-velocity = 3
-radius = 2.5
-position = [0, 0]
-growth_rate = 1
+# velocity = 3
+# radius = 2.5
+# position = [0, 0]
+# growth_rate = 1
 
-theta = np.linspace(0, 2*np.pi, 100)
+# theta = np.linspace(0, 2*np.pi, 100)
 
-for time in range(10):
-    position[0] = velocity * time
+# for time in range(10):
+#     position[0] = velocity * time
 
-    if growth_rate < 1.2 :
-        growth_rate += 0.14 * time
+#     if growth_rate < 1.2 :
+#         growth_rate += 0.14 * time
 
-    x =  position[0] + growth_rate * radius * np.cos(theta)
-    y =  position[1] + growth_rate * radius * np.sin(theta)
+#     x =  position[0] + growth_rate * radius * np.cos(theta)
+#     y =  position[1] + growth_rate * radius * np.sin(theta)
 
-    ax.plot(x, y)
-    ax.plot(position[0], position[1], "*")
+#     ax.plot(x, y)
+#     ax.plot(position[0], position[1], "*")
 
-    plt.pause(1)
-    # ax.clear()
+#     plt.pause(1)
+#     # ax.clear()
+# plt.show()
 
-plt.show()
+
+def noise(position, velocity, radius, time_horizon, growth_rate = 1):
+    theta = np.linspace(0, 2*np.pi, 100)
+
+    for time in range(time_horizon):
+        position[0] = velocity * time       # for time being consider movement only in x direction, change later
+
+        if growth_rate < 1.4:
+            growth_rate += velocity * (time/50)
+
+        x =  position[0] + growth_rate * radius * np.cos(theta)
+        y =  position[1] + growth_rate * radius * np.sin(theta)
+
+        ax.plot(x, y)
+        ax.plot(position[0], position[1], "*") 
+
+        plt.pause(0.5)
+
+if __name__ == "__main__":
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+
+    noise([0, 0], 3, 2.5, 10, 1)
+
+    plt.show()
---
+++ src/New/circular_overlap.py
@@ -0,0 +1,2 @@
+from New.circular_noise import *
+from New.intersection import intersectionArea
