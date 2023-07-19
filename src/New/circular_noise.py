39bf105ea00c63ef1326bc91fe5ba55aaef24171
dikshant-honda <dikshant_s@jp.honda>
2023-07-19 16:26:47 +0900

    better way to allocate points added

--- src/New/circular_noise.py
+++ src/New/circular_noise.py
@@ -27,9 +27,7 @@ def add_noise(
 
     for time in range(time_horizon):
         # change later with IDM predicted points
-        future_points = [[], []]
-        future_points[0].append(position[0] + velocity[0] * time)
-        future_points[1].append(position[1] + velocity[1] * time)
+        future_points = position[0] + velocity[0] * time, position[1] + velocity[1] * time
 
         if growth_rate < 1.4:
             growth_rate += np.sqrt(velocity[0]**2 + velocity[1]**2) * (time/50)
