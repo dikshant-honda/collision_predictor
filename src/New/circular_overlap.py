cf8c479d5a4fa31b2ef4611243b68676b6ab21ec
dikshant-honda <dikshant_s@jp.honda>
2023-07-19 17:36:03 +0900

    formatting done

--- src/New/circular_overlap.py
+++ src/New/circular_overlap.py
@@ -1,10 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from New.circular_noise import add_noise
 from New.intersection import intersectionArea
 
-import numpy as np
-import matplotlib.pyplot as plt
-
-# replace this later 
+# replace this later
 vehicle1_pos = [0, 0]
 vehicle1_vel = [2, 0]
 vehicle1_radius = 2.5
@@ -26,15 +26,18 @@ ax.axis('equal')
 for time in range(time_horizon):
     ax.clear()
     overlap = intersectionArea(noise1[time][0][0], noise1[time][0][1], noise1[time][1],
-                     noise2[time][0][0], noise2[time][0][1], noise2[time][1])
-    
-    normalized_overlap = overlap / (np.pi*((noise1[time][1])**2 + (noise2[time][1])**2))
+                               noise2[time][0][0], noise2[time][0][1], noise2[time][1])
+
+    normalized_overlap = overlap / \
+        (np.pi*((noise1[time][1])**2 + (noise2[time][1])**2))
 
     print(overlap, normalized_overlap)
 
     # visualization
-    ax.plot(noise1[time][0][0]+noise1[time][1]*np.cos(theta), noise1[time][0][1]+noise1[time][1]*np.sin(theta))
-    ax.plot(noise2[time][0][0]+noise2[time][1]*np.cos(theta), noise2[time][0][1]+noise2[time][1]*np.sin(theta))
+    ax.plot(noise1[time][0][0]+noise1[time][1]*np.cos(theta),
+            noise1[time][0][1]+noise1[time][1]*np.sin(theta))
+    ax.plot(noise2[time][0][0]+noise2[time][1]*np.cos(theta),
+            noise2[time][0][1]+noise2[time][1]*np.sin(theta))
     plt.pause(1)
 
 plt.show()
