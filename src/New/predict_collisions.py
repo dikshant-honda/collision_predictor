c76ebcf4608deaf0a7a51713540fcdc0e3afdd0a
dikshant-honda <dikshant_s@jp.honda>
2023-07-21 16:59:50 +0900

    ! infinite acceleration error :( !

--- src/New/predict_collisions.py
+++ src/New/predict_collisions.py
@@ -1,30 +1,47 @@
 #! /usr/bin/env python3
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from IDM.idm import IDM, predict_trajectory
 from New.circular_noise import add_noise
-from New.circular_overlap import overlap
-
+from New.circular_overlap import overlap, plotter
 
 if __name__ == "__main__":
 
-    time_horizon = 50
+    fig, ax = plt.subplots()
+    ax.axis('equal')
+
+    time_horizon = 10
     time_step = 0.1
-
-    # initializations
-    ego_position = 0
-    ego_speed = 15
-
-    lead_position = 50
-    lead_speed = 10
 
     # calling the IDM class object
     idm = IDM()
 
+    # initializations
+    ego_position = 0
+    ego_speed = 20
+    ego_radius = 0.5
 
-    result = predict_trajectory(
+    lead_position = 5
+    lead_speed = 10
+    lead_radius = 0.5
+
+    time, ego_trajectory, lead_trajectory = predict_trajectory(
         idm, ego_position, ego_speed, lead_position, lead_speed, time_horizon, time_step)
 
-    time, ego_trajectory, lead_trajectory = result
+    ego_predictions_with_noise = add_noise(
+        time, ego_trajectory, ego_speed, ego_radius)
 
+    lead_predictions_with_noise = add_noise(
+        time, lead_trajectory, lead_speed, lead_radius)
+
+    for time in range(time_horizon):
+        overlap_area = overlap(
+            ego_predictions_with_noise[time], lead_predictions_with_noise[time])
+        plotter(
+            ax, ego_predictions_with_noise[time], lead_predictions_with_noise[time])
+
+        print("collision probability:", overlap_area)
+
+    plt.show()
