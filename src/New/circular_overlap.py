586b1e6db82dca87cb7ad22217c54f35b8f43439
dikshant-honda <dikshant_s@jp.honda>
2023-07-21 15:30:56 +0900

    one step overlap test done

--- src/New/circular_overlap.py
+++ src/New/circular_overlap.py
@@ -10,8 +10,8 @@ from New.intersection import intersectionArea
 
 def noise(
         vehicle1_location: float,
-        vehicle2_location: float,
         vehicle1_velocity: float,
+        vehicle2_location: float,
         vehicle2_velocity: float,
         time_horizon: float,
         time_step: float,
@@ -44,114 +44,101 @@ def noise(
 
 
 def plotter(
-        vehicle1_location: float,
-        vehicle2_location: float,
-        vehicle1_velocity: float,
-        vehicle2_velocity: float,
-        time_horizon: float,
-        time_step: float,
+        vehicle_1_data: NDArray[np.float64],
+        vehicle_2_data: NDArray[np.float64],
 ) -> None:
     """
     Function to visualize the circular overlap dynamically at every time step
 
     args:
-        vehicle1, vehicle2: pair of traffic vehicles considered for collision check
-        time_horizon: future predictions time horizon
-        time_step: dynamics update rate
+        ax: plotting tool
+        vehicle_1_data: future trajectory information with uncertainity size for vehicle 1, 
+        vehicle_2_data: future trajectory information with uncertainity size for vehicle 2
     """
-    fig, ax = plt.subplots()
-    # ax.axis('equal')
 
     ax.clear()
 
     # visualization parameters
-    # ax.set_xlabel("x(m)")
-    # ax.set_ylabel("y(m)")
-    # ax.set_xlim(-1, 150)
-    # ax.set_ylim(-1, 1)
+    ax.set_xlabel("x(m)")
+    ax.set_ylabel("y(m)")
+    ax.set_xlim(-1, 50)
+    ax.set_ylim(-1.4, 1.4)
 
     theta = np.linspace(0, 2 * np.pi, 100)
 
-    vehicle_1_predictions_with_noise, vehicle_2_predictions_with_noise = noise(
-        vehicle1_location, vehicle1_velocity, vehicle2_location, vehicle2_velocity, time_horizon, time_step)
+    vehicle_1_centers = [vehicle_1_data[0], 0]
+    vehicle_1_size = vehicle_1_data[1]
 
-    for time in range(time_horizon):
-        # ax.clear()
-        # # visualization parameters
-        # ax.set_xlabel("x(m)")
-        # ax.set_ylabel("y(m)")
-        # ax.set_xlim(-1, 50)
-        # ax.set_ylim(-1, 1)
+    vehicle_2_centers = [vehicle_2_data[0], 0]
+    vehicle_2_size = vehicle_2_data[1]
 
-        vehicle_1_centers = [vehicle_1_predictions_with_noise[time][0], 0]
-        vehicle_1_size = vehicle_1_predictions_with_noise[time][1]
+    # plot
+    ax.plot(vehicle_1_centers[0] + vehicle_1_size * np.cos(theta),
+            vehicle_1_centers[1] + vehicle_1_size * np.sin(theta), 'r')
 
-        vehicle_2_centers = [vehicle_2_predictions_with_noise[time][0], 0]
-        vehicle_2_size = vehicle_2_predictions_with_noise[time][1]
+    ax.plot(vehicle_2_centers[0] + vehicle_2_size * np.cos(theta),
+            vehicle_2_centers[1] + vehicle_2_size * np.sin(theta), 'b')
 
-        ax.plot(vehicle_1_centers[0], vehicle_1_centers[1])
-        # plot
-        ax.plot(vehicle_1_centers[0] + vehicle_1_size * np.cos(theta),
-                vehicle_1_centers[1] + vehicle_1_size * np.sin(theta))
-
-        ax.plot(vehicle_2_centers[0] + vehicle_2_size * np.cos(theta),
-                vehicle_2_centers[1] + vehicle_2_size * np.sin(theta))
-
-        plt.draw()
-        # plt.pause(0.5)
-
-    plt.show()
-
+    plt.draw()
+    plt.pause(0.1)
     return
 
 
-# def overlap(
-#         vehicle1: Traffic,
-#         vehicle2: Traffic,
-#         time_horizon: float,
-# ) -> NDArray(np.float64):
-#     """
-#     Function to compute the normalized overlap area of two interescting circles
-#     at every prediction time step
+def overlap(
+        vehicle_1_data: list,
+        vehicle_2_data: list,
+) -> float:
+    """
+    Function to compute the normalized overlap area of two interescting circles
+    at every prediction time step
 
-#     args:
-#         vehicle1, vehicle2: pair of traffic vehicles considered for collision check
-#         time_horizon: future predictions time horizon
-#     """
+    args:
+        vehicle_1_data: future trajectory information with uncertainity size for vehicle 1, 
+        vehicle_2_data: future trajectory information with uncertainity size for vehicle 2
+    """
 
-#     vehicle_1_predictions_with_noise, vehicle_2_predictions_with_noise = noise(
-#         vehicle1, vehicle2, time_horizon)
+    vehicle_1_centers = [vehicle_1_data[0], 0]
+    vehicle_1_size = vehicle_1_data[1]
 
-#     overlap_with_time_step = []
+    vehicle_2_centers = [vehicle_2_data[0], 0]
+    vehicle_2_size = vehicle_2_data[1]
 
-#     for time in range(time_horizon):
-#         vehicle_1_centers = vehicle_1_predictions_with_noise[time][0]
-#         vehicle_1_size = vehicle_1_predictions_with_noise[time][1]
+    overlap = intersectionArea(vehicle_1_centers[0], vehicle_1_centers[1], vehicle_1_size,
+                               vehicle_2_centers[0], vehicle_2_centers[1], vehicle_2_size)
 
-#         vehicle_2_centers = vehicle_2_predictions_with_noise[time][0]
-#         vehicle_2_size = vehicle_2_predictions_with_noise[time][1]
+    normalized_overlap = overlap / \
+        (np.pi * (vehicle_1_size**2 + vehicle_2_size**2))
 
-#         overlap = intersectionArea(vehicle_1_centers[0], vehicle_1_centers[1], vehicle_1_size,
-#                                    vehicle_2_centers[0], vehicle_2_centers[1], vehicle_2_size)
+    return normalized_overlap
 
-#         normalized_overlap = overlap / \
-#             (np.pi * (vehicle_1_size**2 + vehicle_2_size**2))
-
-#         overlap_with_time_step.append(time, normalized_overlap)
-
-#     return overlap_with_time_step
 
 if __name__ == "__main__":
+
+    fig, ax = plt.subplots()
+    ax.axis('equal')
+
+"""
+sample test code for two `non-interacting vehicles`
 
     time_horizon = 30
     time_step = 0.1
 
-    # initializations 1
+    # initializations for vehicle 1
     ego_position_1 = 0
-    ego_speed_1 = 5
+    ego_speed_1 = 20
 
-    # initializations 2
-    ego_position_2 = 10
+    # initializations for vehicle 2
+    ego_position_2 = 5
     ego_speed_2 = 10
  
-    plotter(ego_position_1, ego_position_2, ego_speed_1, ego_speed_2, time_horizon, time_step)    
+    vehicle_1_predictions_with_noise, vehicle_2_predictions_with_noise = noise(
+        ego_position_1, ego_speed_1, ego_position_2, ego_speed_2, time_horizon, time_step)
+
+    for time in range(time_horizon):
+        overlap_area = overlap(vehicle_1_predictions_with_noise[time], vehicle_2_predictions_with_noise[time])
+        plotter(ax, vehicle_1_predictions_with_noise[time], vehicle_2_predictions_with_noise[time]) 
+
+        print("collision probability:", overlap_area)
+
+    plt.show()   
+"""
