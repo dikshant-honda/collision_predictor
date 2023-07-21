4ca0d8b9b04a002967fe228d018308c749ee98ce
dikshant-honda <dikshant_s@jp.honda>
2023-07-21 14:07:21 +0900

    added IDM predictions

--- src/New/circular_noise.py
+++ src/New/circular_noise.py
@@ -1,41 +1,48 @@
 import numpy as np
 from numpy.typing import NDArray
 
-from IDM.idm import IDM
+from IDM.idm import IDM, predict_trajectory
 
 
 def add_noise(
-        position: NDArray[np.float64],
-        velocity: NDArray[np.float64],
+        ego_position: float,
+        ego_speed: float,
+        lead_position: float,
+        lead_speed: float,
         radius: float,
         time_horizon: int,
-        growth_rate: float = 1.0
+        time_step: float,
+        growth_rate: float = 1.0,
 ) -> list:
     """
     function to add circular noise to the future predicted position of the 
-    vehicle assuming constant velocity, further replace by IDM.
+    vehicle assuming constant speed, further replace by IDM.
 
     returns a list of circular noise and time step: [noise(x), noise(y), time]
 
     args:
-        position: current position of the vehicle
-        velocity: current velcoity of the vehicle
+        ego_position: current position of the vehicle
+        ego_speed: current speed of the vehicle
+        lead_position: current position of the vehicle
+        lead_speed: current speed of the vehicle
         radius: noise size, initialized rouhgly by the size of the vehicle
-        time_horizon: time forizon for which future trajectory is needed
+        time_horizon: time horizon for which future trajectory is needed
+        time_step: dynamics update rate
         growth_rate: growth rate of the noise due to increase in uncertainity in the future
     """
 
     noise = []
 
-    for time in range(time_horizon):
-        # change later with IDM predicted points
-        future_points = position[0] + velocity[0] * time, position[1] + velocity[1] * time
+    idm = IDM()
 
+    time_steps, trajectory, _ = predict_trajectory(idm, ego_position, ego_speed, lead_position, lead_speed, time_horizon, time_step)
+
+    for time in range(len(time_steps)):
         if growth_rate < 1.4:
-            growth_rate += np.sqrt(velocity[0]**2 + velocity[1]**2) * (time/50)
+            growth_rate += ego_speed * (time/50)
     
         size = growth_rate * radius 
 
-        noise.append([future_points, size])
+        noise.append([trajectory[time], size])
 
     return noise
