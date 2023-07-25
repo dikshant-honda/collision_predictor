e8f5d9b260a00b2aebf9545ef96eda0e2e359324
dikshant-honda <dikshant_s@jp.honda>
2023-07-25 15:03:41 +0900

    path class ready

--- src/IDM/path.py
+++ src/IDM/path.py
@@ -4,7 +4,11 @@ from IDM.frenet import Point2D
 
 
 class Path:
-    def __init__(self, x: list, y: list) -> None:
+    def __init__(
+            self,
+            x: list,
+            y: list,
+    ) -> None:
         self.x = x
         self.y = y
         self.number_of_points = 100
@@ -12,12 +16,12 @@ class Path:
     def points_to_coordinates(
             self,
             points: list,
-    ):
+    ) -> list:
         """
         Function to convert 2D points to list of coordinates
 
         args:
-            ponts: list of points stored in point2D format
+            points: list of points stored in point2D format
         """
         x_coordinates, y_coordinates = [], []
 
@@ -27,14 +31,26 @@ class Path:
 
         return x_coordinates, y_coordinates
 
-    def coordinates_to_points(self, x, y):
+    def coordinates_to_points(
+        self,
+        x: list,
+        y: list,
+    ) -> list:
+        """
+        Function to convert the separates x,y coordinates list to points format
+        """
         path = []
         for i in range(self.number_of_points):
             path.append(Point2D(x[i], y[i]))
 
         return path
 
-    def get_path(self):
+    def get_path(
+        self
+    ) -> list:
+        """
+        Function just to get the finally Point2D path list
+        """
         path = self.coordinates_to_points(self.x, self.y)
 
         return path
