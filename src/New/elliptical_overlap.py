40fe235b7e9b42e6a702f71a78b8a423d0f195db
dikshant-honda <dikshant_s@jp.honda>
2023-07-26 13:46:22 +0900

    ellipse overlap modifications

--- src/New/elliptical_overlap.py
+++ src/New/elliptical_overlap.py
@@ -1,26 +1,60 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from shapely import MultiPoint, Polygon
 from shapely.geometry.polygon import LinearRing
-from shapely import Polygon
 
-def ellipse_polyline(ellipses, n=100):
-    t = np.linspace(0, 2*np.pi, n, endpoint=False)
-    st = np.sin(t)
-    ct = np.cos(t)
+
+def ellipse_polyline(
+        ellipses: list, 
+        n: int = 100,
+        ) -> list:
+    """
+    Function to draw an ellipse from the ellipses params
+
+    Return the list of elliptical coordinates
+
+    args:
+        ellipses: list of ellipse parameters -> [center.x, center.y, major_axis, minor_axis, orientation]
+        n: number of points 
+    """
+    theta = np.linspace(0, 2*np.pi, n, endpoint=False)
+    st = np.sin(theta)
+    ct = np.cos(theta)
     result = []
-    for x0, y0, a, b, angle in ellipses:
+
+    for x, y, a, b, angle in ellipses:
         angle = np.deg2rad(angle)
         sa = np.sin(angle)
         ca = np.cos(angle)
         p = np.empty((n, 2))
-        p[:, 0] = x0 + a * ca * ct - b * sa * st
-        p[:, 1] = y0 + a * sa * ct + b * ca * st
+        p[:, 0] = x + a * ca * ct - b * sa * st
+        p[:, 1] = y + a * sa * ct + b * ca * st
         result.append(p)
+
     return result
+
+
+def overlap_area(
+        intersection_points: MultiPoint,
+        ) -> float:
+    """
+    Function to compute the area of the onverlapped trapezium
+
+    args:
+        intersection_points: Multipoint list of intersected points
+    """
+    points = []
+    for point in intersection_points.geoms:
+        points.append([point.x, point.y])
+
+    return Polygon(points).area
 
 def intersections(a, b):
     ea = LinearRing(a)
     eb = LinearRing(b)
+
+    print(Polygon(ea).area)
+    print(Polygon(eb).area)
     mp = ea.intersection(eb)
 
     pts = []
