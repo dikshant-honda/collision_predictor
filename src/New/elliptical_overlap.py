898f3c3ceb22a9c90b96b94d5d1457b8f0275e9a
dikshant-honda <dikshant_s@jp.honda>
2023-07-28 18:21:34 +0900

    added overlap function

--- src/New/elliptical_overlap.py
+++ src/New/elliptical_overlap.py
@@ -118,32 +118,70 @@ def collision_probability(
     """
     return overlap / total
 
+
+def overlap(
+        vehicle_1_data: list,
+        vehicle_2_data: list,
+) -> float:
+    """
+    Function to compute the normalized overlap area of two interescting circles
+    at every prediction time step
+
+    args:
+        vehicle_1_data: future trajectory information with uncertainity size for vehicle 1, 
+        vehicle_2_data: future trajectory information with uncertainity size for vehicle 2
+    """
+    vehicle_1_centers = vehicle_1_data[0]
+    vehicle_1_size = vehicle_1_data[1]
+
+    vehicle_2_centers = vehicle_2_data[0]
+    vehicle_2_size = vehicle_2_data[1]
+
+    vehicle_1_params = (vehicle_1_centers.x, vehicle_1_centers.y, vehicle_1_size[0], vehicle_1_size[1], vehicle_1_size[2])
+    vehicle_2_params = (vehicle_2_centers.x, vehicle_2_centers.y, vehicle_2_size[0], vehicle_2_size[1], vehicle_2_size[2])
+
+    vehicle_1, vehicle_2 = ellipse_polyline([vehicle_1_params, vehicle_2_params])
+
+    intersect = intersection_points(vehicle_1, vehicle_2)
+
+    overlap_ = overlap_area(intersect)
+
+    vehicle_1_variance = ellipse_area(vehicle_1)
+    vehicle_2_variance = ellipse_area(vehicle_2)
+
+    total_variance = vehicle_1_variance + vehicle_2_variance
+
+    probability = collision_probability(overlap_, total_variance)
+
+    return probability
+
+
 # """
 # testing code:
 
 
-if __name__ == "__main__":
-    ellipse_1_params = (1, 1, 2, 1, 45)
-    ellipse_2_params = (2, 0.5, 5, 1.5, -30)
+# if __name__ == "__main__":
+#     ellipse_1_params = (1, 1, 2, 1, 45)
+#     ellipse_2_params = (2, 0.5, 5, 1.5, -30)
 
-    ellipse_1, ellipse_2 = ellipse_polyline(
-        [ellipse_1_params, ellipse_2_params])
+#     ellipse_1, ellipse_2 = ellipse_polyline(
+#         [ellipse_1_params, ellipse_2_params])
 
-    plot(ellipse_1, ellipse_2)
+#     plot(ellipse_1, ellipse_2)
 
-    intersect = intersection_points(ellipse_1, ellipse_2)
+#     intersect = intersection_points(ellipse_1, ellipse_2)
 
-    overlap = overlap_area(intersect)
+#     overlap = overlap_area(intersect)
 
-    ellipse_1_area = ellipse_area(ellipse_1)
-    ellipse_2_area = ellipse_area(ellipse_2)
+#     ellipse_1_area = ellipse_area(ellipse_1)
+#     ellipse_2_area = ellipse_area(ellipse_2)
 
-    total_area = ellipse_1_area + ellipse_2_area
+#     total_area = ellipse_1_area + ellipse_2_area
 
-    prob = collision_probability(overlap, total_area)
+#     prob = collision_probability(overlap, total_area)
 
-    print(prob)
+#     print(prob)
 
-    plt.show()
+#     plt.show()
 
 # """
