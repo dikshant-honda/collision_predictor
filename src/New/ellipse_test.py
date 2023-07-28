87467a76742c16309a391b24ede1b245899dace8
dikshant-honda <dikshant_s@jp.honda>
2023-07-28 20:26:27 +0900

    perfect code ready for getting overlap and visualization as well

--- src/New/ellipse_test.py
+++ src/New/ellipse_test.py
@@ -17,19 +17,35 @@ def ellipse(center_x, center_y, major_axis, minor_axis, rotation_angle=0):
     return ellipse
 
 def overlap_area(ellipse1, ellipse2):
-    # Find the intersection of the two ellipses
-    intersection = ellipse1.intersection(ellipse2)
+    # # Find the intersection of the two ellipses
+    # intersection = ellipse1.intersection(ellipse2)
+
+    # # Calculate the area of the intersection
+    # area = intersection.area
+    if ellipse1.intersects(ellipse2): 
+        intersect = ellipse1.intersection(ellipse2).area
+        return intersect
+    return 0
     
-    # Calculate the area of the intersection
-    area = intersection.area
-    
-    return area
+    # return area
 
 # Example usage
-ellipse1 = ellipse(center_x=0, center_y=0, major_axis=5, minor_axis=3, rotation_angle=45)
-ellipse2 = ellipse(center_x=2, center_y=2, major_axis=4, minor_axis=2)
+ellipse1 = ellipse(center_x=1, center_y=1, major_axis=6, minor_axis=2, rotation_angle=0)
+ellipse2 = ellipse(center_x=15, center_y=0, major_axis=5, minor_axis=1.5)
 
-# plt.plot(ellipse1[:, 0], ellipse1[:, 1])
+xx, yy = ellipse1.exterior.coords.xy
+
+x = xx.tolist()
+y = yy.tolist()
+
+plt.plot(x, y)
+
+xx, yy = ellipse2.exterior.coords.xy
+
+x = xx.tolist()
+y = yy.tolist()
+
+plt.plot(x, y)
 
 overlap_area = overlap_area(ellipse1, ellipse2)
 print("Overlap Area:", overlap_area)
