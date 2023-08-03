8e097a613b80e170782a0b4984866578c203d809
dikshant-honda <dikshant_s@jp.honda>
2023-08-03 17:26:20 +0900

    add turning lane

--- src/env_info/cross_intersection.py
+++ src/env_info/cross_intersection.py
@@ -7,14 +7,17 @@ number_of_points = 300
 
 
 def plot():
+    # lane centers
     plt.plot(x_horizontal_lane, y_horizontal_lane, 'r--')
-    plt.plot(x_vertical_lane, y_vertical_lane, 'r--')
+    plt.plot(x_vertical_lane, y_vertical_lane, 'g--')
+    plt.plot(x_turning, y_turning, 'c--')
 
-    plt.plot(x_horizontal_lane, boundaries_left_lane, 'b-')
-    plt.plot(x_horizontal_lane, boundaries_right_lane, 'b-')
+    # boundaries
+    plt.plot(x_horizontal_lane, boundaries_left_lane, 'k-')
+    plt.plot(x_horizontal_lane, boundaries_right_lane, 'k-')
 
-    plt.plot(boundaries_left_lane, y_vertical_lane, 'g-')
-    plt.plot(boundaries_right_lane, y_vertical_lane, 'g-')
+    plt.plot(boundaries_left_lane, y_vertical_lane, 'k-')
+    plt.plot(boundaries_right_lane, y_vertical_lane, 'k-')
 
     plt.axis('equal')
 
@@ -30,6 +33,15 @@ y_horizontal_lane = np.linspace(0, 0, number_of_points)
 # vertical
 x_vertical_lane = np.linspace(0, 0, number_of_points)
 y_vertical_lane = np.linspace(-150, 150, number_of_points)
+
+# turning lane
+x_half_horizontal = np.linspace(-150, 0, number_of_points//2)
+y_half_horizontal = np.linspace(0, 0, number_of_points//2)
+x_half_vertical = np.linspace(0, 0, number_of_points//2)
+y_half_vertical = np.linspace(0, 150, number_of_points//2)
+
+x_turning = np.hstack((x_half_horizontal, x_half_vertical))
+y_turning = np.hstack((y_half_horizontal, y_half_vertical))
 
 # lane boundaries
 boundaries_left_lane = np.linspace(5, 5, number_of_points)
