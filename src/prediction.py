e70135afa1ef9edb74e5829b4355610880177a98
dikshant_honda <dikshant_s@jp.honda>
2024-04-01 11:35:02 +0900

    integration with detection module begins

--- src/prediction.py
+++ src/prediction.py
@@ -9,10 +9,7 @@ from collision_predictor.msg import vehicle, environment
 
 class Predictions:
     def __init__(self) -> None:
-        self.predictions_pub = rospy.Publisher('prediction_info', environment, queue_size=10)
-        rospy.init_node('future trajectory predictions')
-        self.rate = rospy.Rate(10000)
-        self.sub = rospy.Subscriber('env_info', environment, self.callback)
+        self.sub = rospy.Subscriber('env', environment, self.callback)
 
     def callback(self, data):
         self.env = data
@@ -21,8 +18,6 @@ class Predictions:
     def get_vehicle_data(self, env: environment):
         for vehicle_info in env.vehicle_states:
             vehicle_info = self.get_future_trajectory(vehicle_info)
-        self.predictions_pub.publish(env)
-        self.rate.sleep()
 
     def get_future_trajectory(self, vehicle_info: vehicle):
         '''
@@ -32,12 +27,16 @@ class Predictions:
         3. using frenet function to make use of the lane information
         4. using intelligent driver model (IDM)
         '''
-        vehicle_info.future_trajectory.append(Point(vehicle_info.x_position, vehicle_info.y_position, 0))
+        vehicle_info.future_trajectory.append(
+            Point(vehicle_info.x_position, vehicle_info.y_position, 0))
         return vehicle_info
 
+
 if __name__ == '__main__':
-    # subscriber node for detection
-    rospy.init_node('update environment', anonymous=True)
+    # publisher node for predictions
+    rospy.init_node('predictions', anonymous=True)
+    env_pub = rospy.Publisher('predict', environment, queue_size=10)
+
     Predictions()
 
     try:
