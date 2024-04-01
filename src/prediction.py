45c10bc44d5e47e72309762396a83ff9ad0b4e8c
dikshant_honda <dikshant_s@jp.honda>
2024-04-01 14:51:02 +0900

    added markers for predictions, bug examined

--- src/prediction.py
+++ src/prediction.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from geometry_msgs.msg import Point
 from collision_predictor.msg import vehicle, environment
+from visualization_msgs.msg import Marker
 
 
 class Predictions:
@@ -30,13 +31,14 @@ class Predictions:
         '''
         vehicle_info.future_trajectory = self.update(
             vehicle_info.x_position.data, vehicle_info.y_position.data, vehicle_info.direction.data, vehicle_info.speed.data)
+        self.mark(vehicle_info.id, vehicle_info.direction, vehicle_info.future_trajectory)
         return vehicle_info
 
     def update(self, x, y, direction, speed):
         f_x, f_y = [], []
         for t in range(10):
-            x = x + direction * speed * t * 0.005
-            y = y + direction * np.random.randn() * t * 0.005
+            x = x + direction * np.random.randn() * t * 0.005
+            y = y + direction * speed * t * 0.005
             f_x.append(x)
             f_y.append(y)
         points = self.ls_to_point(f_x, f_y)
@@ -48,12 +50,42 @@ class Predictions:
             points.append(Point(ls_x[i], ls_y[i], 0))
         return points
 
+    def mark(self, id, direction, trajectory):
+        for point in trajectory:
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.header.stamp = rospy.get_rostime()
+            marker.ns = "vehicle"
+            marker.id = id.data
+            marker.pose.position.x = point.x
+            marker.pose.position.y = point.y
+            marker.pose.position.z = 0
+            marker.pose.orientation.x = 0
+            marker.pose.orientation.y = 0
+            marker.pose.orientation.z = 0
+            marker.pose.orientation.w = direction.data
+            marker.scale.x = 50.0
+            marker.scale.y = 50.0
+            marker.scale.z = 50.0
+
+            if direction.data == 1:
+                marker.color.r = 0.0
+                marker.color.g = 0.0
+                marker.color.b = 1.0
+            else:
+                marker.color.r = 0.0
+                marker.color.g = 1.0
+                marker.color.b = 0.0
+            marker.color.a = 1.0
+            marker.lifetime.nsecs = 75000000
+            markerPub.publish(marker)
+
 
 if __name__ == '__main__':
     # publisher node for predictions
     rospy.init_node('predictions', anonymous=True)
     env_pub = rospy.Publisher('predict', environment, queue_size=10)
-
+    markerPub = rospy.Publisher('vehicle', Marker, queue_size=10)
     Predictions()
 
     try:
