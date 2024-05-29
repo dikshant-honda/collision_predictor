110af80b54767e349e6d57463c969fbfaf3f2c6e
dikshant_honda <dikshant_s@jp.honda>
2024-05-29 14:52:01 +0900

    remove dependency from the integration and added lane directly

--- src/prediction.py
+++ src/prediction.py
@@ -5,20 +5,22 @@ import numpy as np
 import bisect
 
 from geometry_msgs.msg import Point
-from collision_predictor.msg import vehicle, environment
+from collision_predictor.msg import vehicle
 from visualization_msgs.msg import Marker
+from multi_vehicle_tracking.msg import queue
 
 
 class Predictions:
     def __init__(self) -> None:
-        self.sub = rospy.Subscriber('env', environment, self.callback)
+        self.sub = rospy.Subscriber('env_info', queue, self.callback)
+        self.lanes = Lanes()
 
     def callback(self, data):
         self.env = data
         self.get_vehicle_data(self.env)
 
-    def get_vehicle_data(self, env: environment):
-        for vehicle_info in env.vehicle_states:
+    def get_vehicle_data(self, env: queue):
+        for vehicle_info in env.info:
             vehicle_info = self.get_future_trajectory(vehicle_info)
         env_pub.publish(env)
 
@@ -33,7 +35,6 @@ class Predictions:
             vehicle_info.x_position.data, vehicle_info.y_position.data, vehicle_info.direction.data, vehicle_info.speed.data)
         self.mark(vehicle_info.id, vehicle_info.direction,
                   vehicle_info.future_trajectory)
-        self.markLanes(vehicle_info)
         return vehicle_info
 
     def point_on_lane(self, start, end, point):
@@ -88,6 +89,46 @@ class Predictions:
         markerPub.publish(marker)
         rate.sleep()
 
+
+class Lanes:
+    def __init__(self,) -> None:
+        """
+        class for accessing the lane information
+
+        args:
+            mid_lanes: start and end points of the mid lanes. as of now, they are hard-coded because we have not done camera calibration
+        """
+        self.mid_lanes = np.flip(np.array([[418.17932825, -458.47816553, 428.60006397, -168.4146595],
+                                           [468.05591932, -439.93474339,
+                                               496.06556125, -189.02849181],
+                                           [730.6705301, 75.1316708,
+                                               215.12283138, 73.98304454],
+                                           [730.85281032, 254.21177936, 84.52455505, 257.48031548]]))
+        # self.markLanes()
+
+    def get_lane(self, vehicle):
+        for line in self.mid_lanes:
+            p1 = np.array([line[0], line[1]])
+            p2 = np.array([line[2], line[3]])
+            p3 = np.array([vehicle.x_position,
+                           vehicle.y_position])
+            dist = abs(np.cross(p2-p1, p3-p1)/np.linalg.norm(p2-p1))
+            if dist < d:
+                start_point, end_point = Point(), Point()
+                start_point.x, start_point.y = line[0], line[1]
+                end_point.x, end_point.y = line[2], line[3]
+                d = dist
+        return start_point, end_point
+
+    def build_lane(self, start: Point, end: Point, num_points=100):
+        x_ = np.linspace(start.x, end.x, num_points)
+        y_ = np.linspace(start.y, end.y, num_points)
+        # return list(zip(x_, y_))    # update later
+        return x_, y_
+
+    def distance(self, x1, y1, x2, y2):
+        return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+
     def markLanes(self, vehicle_info: vehicle):
         marker = Marker()
         marker.header.frame_id = "map"
@@ -96,7 +137,8 @@ class Predictions:
         marker.type = marker.POINTS
         marker.action = 0
         lane_info = Lanes()
-        lanes_x, lanes_y = lane_info.build_lane(vehicle_info.start_point, vehicle_info.end_point)
+        lanes_x, lanes_y = lane_info.build_lane(
+            vehicle_info.start_point, vehicle_info.end_point)
         lanes = self.ls_to_point(lanes_x, lanes_y)
         marker.points = lanes
         marker.scale.x = 10.0
@@ -109,20 +151,6 @@ class Predictions:
         marker.lifetime.nsecs = 75000000
         markerPub.publish(marker)
         rate.sleep()
-
-
-class Lanes:
-    def __init__(self) -> None:
-        pass
-
-    def build_lane(self, start: Point, end: Point, num_points=100):
-        x_ = np.linspace(start.x, end.x, num_points)
-        y_ = np.linspace(start.y, end.y, num_points)
-        # return list(zip(x_, y_))    # update later
-        return x_, y_
-
-    def distance(self, x1, y1, x2, y2):
-        return np.sqrt((x1-x2)**2 + (y1-y2)**2)
 
     def get_s_map(self, lane):
         s_map = np.array([], dtype=np.float64)
@@ -147,7 +175,7 @@ class Frenet:
 if __name__ == '__main__':
     # publisher node for predictions
     rospy.init_node('predictions', anonymous=True)
-    env_pub = rospy.Publisher('predict', environment, queue_size=10)
+    env_pub = rospy.Publisher('predict', queue, queue_size=10)
     markerPub = rospy.Publisher('vehicle', Marker, queue_size=10)
     centerLaneMarker = rospy.Publisher('lanes', Marker, queue_size=10)
     rate = rospy.Rate(10000)
